52644b8379bab25fb4fcc471791cd2edf6f448db
Aiden D <32815852+4xe5cr6vt7yb8un9@users.noreply.github.com>
2024-07-02 13:54:50 -0400

    Created file for testing anthropic

---
+++ antropic_test.py
@@ -0,0 +1,59 @@
+import anthropic
+import base64
+import httpx
+
+from dotenv import load_dotenv
+from utils import extract_json, log_transcription
+from compare import word_distance
+
+load_dotenv() 
+
+def transcribe_image(url, prompt, client):
+    image1_media_type = "image/jpeg"
+    image1_data = base64.b64encode(httpx.get(url).content).decode("utf-8")
+
+    message = client.messages.create(
+        model="claude-3-5-sonnet-20240620",
+        max_tokens=1024,
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "image",
+                        "source": {
+                            "type": "base64",
+                            "media_type": image1_media_type,
+                            "data": image1_data,
+                        },
+                    },
+                    {
+                        "type": "text",
+                        "text": prompt
+                    }
+                ],
+            }
+        ],
+    )
+    return message
+
+
+test_data = extract_json('testing_data.json')
+
+client = anthropic.Anthropic()
+prompt = "Please transcribe the text from the following image with high accuracy. Ensure that all punctuation, capitalization, and formatting are preserved as closely as possible to the original. If any part of the text is illegible or unclear, indicate this with '[illegible]' in the transcription. Pay special attention to names, dates, and any specific terminology. Provide only the transcription"
+
+data = test_data.get('data', [])[2]
+#for data in test_data.get('data', []):
+url = data.get('image_url')
+print(f"Transcribing image: {url}")
+response = transcribe_image(url, prompt, client)
+extracted_text = response.content[0].text
+print("Finished transcription")
+
+print("Calculating word distance")
+manual_text = data.get("image_text")
+distance = word_distance([extracted_text, manual_text])
+print('The transcription has a distance of %.4F\n' % distance)
+
+log_transcription(extracted_text, response.model, distance, prompt, url)
