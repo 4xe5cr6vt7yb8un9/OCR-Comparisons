b6b44b6514d413fb8f298823e38b63213ee55f74
Aiden D <32815852+4xe5cr6vt7yb8un9@users.noreply.github.com>
2024-07-03 13:48:04 -0400

    Tested using locally hosted Mixtral

--- ollama_test.py
+++ ollama_test.py
@@ -1,35 +1,24 @@
-import json
-import requests
-from PIL import Image
+from utils import log_transcription, extract_json
+from compare import word_distance
 
 from langchain_community.llms import Ollama
 
-def extract_json(file_path):
-    test_data = []
-    # Load the JSON file
-    with open(file_path, 'r') as file:
-        data = json.load(file)
-
-    # Extract and display the relevant information from each document
-    documents = data.get("data", [])
-    for doc in documents:
-        image_url = doc.get("image_url", "No URL provided")
-        image_text = doc.get("image_text", "No text provided")
-        quality = doc.get("quality", "No quality description provided")
-        date = doc.get("date", "No date provided")
-
-        data = [image_url, image_text, quality, date]
-        test_data.append(data)
-
-    return test_data
-
 test_data = extract_json('testing_data.json')
 
-llm = Ollama(model='llama3')
+llm = Ollama(model='mixtral')
 
-url = test_data[0][0]
-image = Image.open(requests.get(url, stream=True).raw).convert("RGB")
-image.show()
+for data in test_data.get('data', []):
+    url = data.get('image_url')
 
-res = llm.invoke("Transcribe this image: "+url)
-print(res)
+    print(f"Transcribing image: {url}")
+    prompt = f"Please transcribe the text from the following image with high accuracy. Ensure that all punctuation, capitalization, and formatting are preserved as closely as possible to the original. If any part of the text is illegible or unclear, indicate this with '[illegible]' in the transcription. Pay special attention to names, dates, and any specific terminology. Please provide only the transcription: {url}"
+    response = llm.invoke(prompt)
+    print("Finished transcription")
+
+    # Calculates the distance between the automatted and manual transcriptions
+    print("Calculating word distance")
+    manual_text = data.get("image_text")
+    distance = word_distance([response, manual_text])
+    print('The transcription has a distance of %.4F\n' % distance)
+
+    log_transcription(response, "Mixtral", distance, prompt, url)
