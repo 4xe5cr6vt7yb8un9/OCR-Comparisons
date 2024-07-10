acf5784371dc42160cadd71ab7594e6b5ab546bf
Aiden D <32815852+4xe5cr6vt7yb8un9@users.noreply.github.com>
2024-07-10 12:37:36 -0400

    updated to get the correct transciptions and to include the date

--- helpers/get_NARA_files.py
+++ helpers/get_NARA_files.py
@@ -1,8 +1,11 @@
 import requests
 import json
+from os.path import exists
+
 
 def extract_digital_objects(naID):
     url = f"https://catalog.archives.gov/proxy/records/search?naId_is={naID}&allowLegacyOrgNames=true&includeExtractedText=true"
+    url2 = f"https://catalog.archives.gov/proxy/contributions/targetNaId/{naID}"
 
     headers = {
         "accept": "*/*",
@@ -10,28 +13,49 @@ def extract_digital_objects(naID):
     }
 
     response = requests.get(url, headers=headers)
+    response2 = requests.get(url2, headers=headers)
     
-    if response.status_code == 200:
+    if response.status_code == 200 and response2.status_code == 200:
         data = response.json()
+        transcript_data = response2.json()
 
         # There is a lot more data in this JSON object I am just including the dict that has the specific file details.
         digital_objects = data["body"]["hits"]["hits"][0]["_source"]["record"]["digitalObjects"]
+        start_date = data["body"]["hits"]["hits"][0]["_source"]["record"]["ancestors"][1]["inclusiveStartDate"]["year"]
+        end_date = data["body"]["hits"]["hits"][0]["_source"]["record"]["ancestors"][1]["inclusiveEndDate"]["year"]
         extracted_objects = []
 
         for obj in digital_objects:
+            id = obj["objectId"]
+            extract = ""
+            for contrib in transcript_data:
+                if contrib["targetObjectId"] == id and contrib["contributionType"] == "transcription":
+                    extract = contrib["contribution"]
+                    break
+
+            if extract == "":
+                continue
+
             extracted_obj = {
                 "objectId": obj["objectId"],
+                "date_range": f"{start_date}-{end_date}",
                 "objectUrl": obj["objectUrl"],
-                "extractedText": obj["extractedText"],
+                "extractedText": extract,
                 "objectDescription": obj["objectDescription"],
                 # ... (Include any other fields you want to extract)
             }
             extracted_objects.append(extracted_obj)
 
-        return json.dumps({"digitalObjects": extracted_objects})  # Return as JSON string
+        output_file = f"documents/{naID}.json"
+            
+        with open(output_file, 'w', encoding='utf-8') as file:
+            json.dump({"digitalObjects": extracted_objects}, file, indent = 4, ensure_ascii=False)
+
     else:
         return json.dumps({"error": f"Request failed with status code: {response.status_code}"})
 
 if __name__ == '__main__':
     # Pass in the NAID
-    print(extract_digital_objects(naID=54953855))
+    ext = extract_digital_objects(naID=54953855)
+
+    
