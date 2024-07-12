02d27fbd79f9b7b5a05c196d29856327643e2c2e
Tim C. <tcondello@users.noreply.github.com>
2024-07-12 15:53:58 -0400

    added a webapp to view the JSON

---
+++ app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__)
+
+# Load JSON data
+with open('../documents/NARA_files.json', 'r') as file:
+    data = json.load(file)
+
+@app.route('/')
+def index():
+    # Extract transcripts from the JSON data
+    transcripts = data.get('digitalObjects')
+    print(data.get('digitalObjects')[0])
+    return render_template('index.html', transcripts=transcripts)
+
+if __name__ == '__main__':
+    app.run(debug=True)
